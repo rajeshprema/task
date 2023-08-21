f5b46497791f6c9f047621ad8ca51db3df25e11f
rajeshprema <122607002+rajeshprema@users.noreply.github.com>
2023-08-21 13:05:42 +0530

    Add files via upload

---
+++ src/pricecart.js
@@ -0,0 +1,62 @@
+
+import './pricecard.css';
+
+
+const Pricecard = () => {
+
+    
+        
+
+
+    return (
+        <div className="Pricecard">
+            <div className="container">
+            <p>F R E E</p>
+            <h1>$0/month</h1><br></br>
+             <hr></hr>
+                <li>Single user </li><br></br>
+                
+               <br></br> 
+            <li>5GB Storage</li><br></br>
+            <li>Unlimited Public Projects</li><br></br>
+            <li>Community Access</li><br></br>
+            <li>Unlimited Private Projects</li><br></br>
+            <li>Dedicated Phone Support</li><br></br>
+            <li>Free Subdomain</li><br></br>
+            <li>Monthly Status Reports</li><br></br>
+            <button>BUTTON</button><br></br>
+            <br></br>
+           </div>
+           <div className="container1">
+            <p>P L U S</p>
+            <h1>$9/month</h1><br></br>
+            <hr></hr> 
+            <h2> <li>5 Users</li> </h2><br></br>
+            <li>50GB Storage</li><br></br>
+            <li>Unlimited Public Projects</li><br></br>
+            <li>Community Access</li><br></br>
+            <li>Unlimited Private Projects</li><br></br>
+            <li>Dedicated Phone Support</li><br></br>
+            <li>Free subdomain</li><br></br>
+            <li>Monthly Status Reports</li><br></br>
+            <button>BUTTON</button>
+            <br></br>
+            </div>
+            <div className="container2">
+            <p>P R O</p>
+            <h1>$49/month</h1><br></br>
+             <hr></hr> 
+            <h2> <li>Unlimited Users</li></h2><br></br>
+            <li>150GB Storage</li><br></br>
+            <li>Unlimited Public Projects</li><br></br>
+            <li>Community Access</li><br></br>
+            <li>Unlimited Private Projects</li><br></br>
+            <li>Dedicated Phone Support</li><br></br>
+            <li>Unlimited Free Subdomain</li><br></br>
+            <li>Monthly Status Reports</li><br></br>
+            <button>BUTTON</button>
+           </div>
+     </div>
+    )
+}
+export default Pricecard;
